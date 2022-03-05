9fd6f80e5cc9333ec09acdb9454c05fd0eb36391
Eddie Barzi <73700112+19eddie@users.noreply.github.com>
2022-03-05 23:47:36 +0100

    Abstract class for bank account

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -5,49 +5,15 @@ package lab01.example.model;
  * In particular, a Simple Bank Account allows always the deposit
  * while the withdrawal is allowed only if the balance greater or equal the withdrawal amount
  */
-public class SimpleBankAccountWithAtm implements BankAccount {
+public class SimpleBankAccountWithAtm extends AbstractBankAccount {
 
-    private double balance;
-    private final AccountHolder holder;
     private static final int FEE = 1;
 
     public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
-        this.holder = holder;
-        this.balance = balance;
-    }
-    @Override
-    public AccountHolder getHolder(){
-        return this.holder;
+        super(balance, holder);
     }
 
-    @Override
-    public double getBalance() {
-        return this.balance;
-    }
-
-    @Override
-    public void deposit(final int userID, final double amount) {
-        if (checkUser(userID) && isDepositAllowed(amount)) {
-            this.balance += amount-FEE;
-        }
-    }
-
-    private boolean isDepositAllowed(final double amount){
-        return FEE <= amount;
-    }
-
-    @Override
-    public void withdraw(final int userID, final double amount) {
-        if (checkUser(userID) && isWithdrawAllowed(amount)) {
-            this.balance -= amount+FEE;
-        }
-    }
-
-    private boolean isWithdrawAllowed(final double amount){
-        return this.balance >= amount+FEE;
-    }
-
-    private boolean checkUser(final int id) {
-        return this.holder.getId() == id;
+    protected int getFee() {
+        return FEE;
     }
 }
